98b11a8eb0bae2fe7493da3cb51241d24821344b
ritisha-18 <rd8791@srmist.edu.in>
2026-03-03 14:52:37 +0530

    Add files via upload

---
+++ practiceproblem3.java
@@ -0,0 +1,15 @@
+public class practiceproblem3 {
+
+    public static void main(String[] args){
+
+        double kilometers = 10.8;
+        double conversion = 1.6;
+
+        double miles = kilometers * conversion;
+
+        System.out.println("the distance "+kilometers+" in miles is "+miles);
+
+    
+    }
+    
+}
